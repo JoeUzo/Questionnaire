6e8323b47094a355a53644403220fca44c0408d2
Joseph <111964629+JoeUzo@users.noreply.github.com>
2024-06-19 11:52:21 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,7 +19,7 @@ app = Flask(__name__)
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY")
 Bootstrap5(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("ADATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
