a67e5a3b06c4f2ca9e08d447a5afea68e3f1ba77
Joe <joeuzoproject@gmail.com>
2024-07-26 16:31:13 +0100

    Update ques.py

--- ques.py
+++ ques.py
@@ -1,12 +1,8 @@
 import requests
 import os
 from dotenv import load_dotenv
-import json
 
 load_dotenv()
 
 questions_url = os.getenv("QUESTION_URL")
 questions = requests.get(url=questions_url).json()
-
-# with open('data.json', 'r') as file:
-#     questions = json.load(file)
